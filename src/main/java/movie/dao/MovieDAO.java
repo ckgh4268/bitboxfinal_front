d6ea3304feeba260ca7c148e40c83e2d8d409ee7
ckgh4268 <qkrcksgh1028@naver.com>
2023-02-16 09:54:31 +0900

    commit

--- src/main/java/movie/dao/MovieDAO.java
+++ src/main/java/movie/dao/MovieDAO.java
@@ -45,7 +45,7 @@ public interface MovieDAO extends JpaRepository<MovieDTO, String>{
 	 
 	@Query("select movieDTO from MovieDTO movieDTO where movieDTO.movie_already_released = '0' AND movieDTO.movie_class = '4' ORDER BY movieDTO.movie_reserve_rate DESC")
 	public List<MovieDTO> getMovieList_already_on_filmsociety();
-	 
+	  
 	@Query("select movieDTO from MovieDTO movieDTO where movieDTO.movie_title like '%' || :keyword || '%' AND movieDTO.movie_class = '4' ORDER BY movieDTO.movie_reserve_rate DESC")
 	public List<MovieDTO> getMovie_title_search_filmsociety(@Param("keyword")String keyword);
 	
